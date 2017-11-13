9625bb2bf9c2885562686c0eac0bce6765ded9f5
alsacchi <andrealorenzosacchi@gmail.com>
2017-11-13 14:22:49 +0100

    EasyHome

---
+++ EasyHome/src/com/alsacchi/easycommand/main.java
@@ -0,0 +1,80 @@
+package com.alsacchi.easycommand;
+
+import org.bukkit.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+
+public class main extends JavaPlugin {
+    private Logger Log = Bukkit.getLogger();
+    private HashMap<String, Location> playerHomes = new HashMap<>();
+    private FileConfiguration config = this.getConfig();
+
+    @Override
+    public void onEnable() {
+        config.addDefault("home", true);
+        config.addDefault("homes", "");
+        config.options().copyDefaults(true);
+        saveConfig();
+        Log.warning("[EasyHome] Activated");
+    }
+
+    @Override
+    public void onDisable() {
+        Log.warning("[EasyHome] Deactivated");
+    }
+
+    public boolean onCommand(CommandSender theSender, Command cmd, String commandLabel, String[] args) {
+        Player player = (Player) theSender;
+        if (theSender != null) {
+            if (commandLabel.equalsIgnoreCase("sethome")) {
+                if (player.hasPermission("easyhome.sethome")) {
+                    Location tmploc = player.getLocation();
+                    playerHomes.put(player.getName(), tmploc);
+                    String tmpWorld = player.getWorld().getName();
+                    double tmpX = tmploc.getX();
+                    double tmpY = tmploc.getY();
+                    double tmpZ = tmploc.getZ();
+                    this.getConfig().set("homes." + String.valueOf(player.getUniqueId()), tmpWorld + "," + tmpX + "," + tmpY + "," + tmpZ);
+                    this.saveConfig();
+                    player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "[EasyHome]" + ChatColor.WHITE + "" + " Home set!");
+                } else {
+                    player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "[EasyHome]" + ChatColor.WHITE + "" + " You don't have the right permission");
+                }
+            } else if (commandLabel.equalsIgnoreCase("home")) {
+                if (player.hasPermission("easyhome.home")) {
+                    if (config.getBoolean("home")) {
+                        if (this.getConfig().contains("homes." + String.valueOf(player.getUniqueId()))) {
+                            String[] arg = this.getConfig().getString("homes." + String.valueOf(player.getUniqueId())).split(",");
+                            double[] parsed = new double[3];
+                            for (int a = 0; a < 3; a++) {
+                                parsed[a] = Double.parseDouble(arg[a + 1]);
+                            }
+                            Location location = new Location(Bukkit.getServer().getWorld(arg[0]), parsed[0], parsed[1], parsed[2]);
+                            playerHomes.put(String.valueOf(player.getUniqueId()), location);
+                            Location homeLocation = playerHomes.get(String.valueOf(player.getUniqueId()));
+                            player.teleport(homeLocation);
+                            player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "[EasyHome]" + ChatColor.WHITE + "" + " Teleported to home!");
+                        } else {
+                            player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "[EasyHome]" + ChatColor.WHITE + "" + " You have to set a house!\n Do /sethome");
+                        }
+                    }
+                } else {
+                    player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "[EasyHome]" + ChatColor.WHITE + "" + " You don't have the right permission");
+                }
+            } else {
+                player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "[EasyHome]" + ChatColor.WHITE + "" + " Plugin deactivated in config file, contact an amministrator");
+            }
+        }
+        return true;
+    }
+}
+
+
